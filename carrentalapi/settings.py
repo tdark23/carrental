6ff2fef2a3abf26ab5430af26d53798726a59be0
tedmbang <ted.mbang@hintekk.com>
2024-01-30 04:50:40 +0100

    added database_url in env

--- carrentalapi/settings.py
+++ carrentalapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-78af%q@$uqix-x_iwfc#86zy9+go@mvsh1dax03l734f=^5^m@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
